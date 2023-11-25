181e5756f2cff8efa500fbd49964bfd1474d0d77
Wietse Wind <mail@wietse.com>
2023-11-26 00:45:06 +0100

    Default account = to activate

--- src/Components/MainPage/Activate/AccountActivation.tsx
+++ src/Components/MainPage/Activate/AccountActivation.tsx
@@ -11,7 +11,7 @@ import iconChevronLeft from '../../../assets/chevron-left.png'
 export default function AccountActivation(props: any) {
     const [chosenAmount, setChosenAmount] = useState<number>(10);
     const [pageIsLoading, setPageIsLoading] = useState(false);
-    const [selectedAccount, setSelectedAccount] = useState<string>('r...');
+    const [selectedAccount, setSelectedAccount] = useState<string>(props.accountToActivate);
     const [selectedAccountName, setSelectedAccountName] = useState<string>('Select source account...');
     const [selectedAccountBalance, setSelectedAccountBalance] = useState<number>(0);
     const [hasAccountError, setHasAccountError] = useState<boolean>(false);
