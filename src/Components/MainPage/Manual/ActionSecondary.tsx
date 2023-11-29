f3980587c43e2375498818bfcc5e917f716b7926
Kevin Koobs <k.koobs@outlook.com>
2023-11-29 14:05:22 +0100

    Revamped homescreen

--- src/Components/MainPage/Manual/ActionSecondary.tsx
+++ src/Components/MainPage/Manual/ActionSecondary.tsx
@@ -7,7 +7,7 @@ export type ActionProps = {
 }
 export default function ActionSecondary(props: ActionProps) {
     return (
-        <div onClick={() => props.onClick()} className="w-full button bg-[rgb(var(--colorLight))] rounded-[20px] text-secondary flex items-center justify-center gap-5 pl-[25px] pr-4">
+        <div onClick={() => props.onClick()} className="w-full button bg-theme-tint rounded-[20px] text-secondary flex items-center justify-center gap-5 pl-[25px] pr-4">
             <img className="m-0" src={props.icon} />
             <p className="flex-grow">{props.title}</p>
             <img className="m-0" src={iconArrowRight} />
