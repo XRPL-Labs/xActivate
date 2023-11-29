60de9af13460a2a762b70f4f755151b502df7474
Kevin Koobs <k.koobs@outlook.com>
2023-11-29 14:19:02 +0100

    Small upadtes to text

--- src/Components/MainPage/Hurray.tsx
+++ src/Components/MainPage/Hurray.tsx
@@ -17,7 +17,7 @@ export default function Hurray(props: any) {
                     <img src={imageActivateAccount} className="w-[40%] mx-auto" />
 
                     <h1 className="text-center text-2xl text-primary">Your account is activated!</h1>
-                    <p className="m-0 text-secondary font-bold">This account is already activated. You can close the xApp and enjoy your Xumm account!</p>
+                    <p className="m-0 text-secondary">This account is already activated. You can close the xApp and enjoy your Xumm account!</p>
                 </div>
                 <Confetti />
                 <div className="fixed max-h-[195px] bg-theme-tint w-full bottom-0 border-t-[1px] border-t-[#EBECEE] flex items-center flex-col gap-4 pt-[22px] pb-[30px] pl-[20px] pr-[20px] left-0">
