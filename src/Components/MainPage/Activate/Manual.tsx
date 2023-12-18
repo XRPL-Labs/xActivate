2b0fbd86c5469571bdea3243f9635669d3c89f24
Wietse Wind <mail@wietse.com>
2023-12-18 14:33:27 +0100

    Change Xahau in-app page

--- src/Components/MainPage/Activate/Manual.tsx
+++ src/Components/MainPage/Activate/Manual.tsx
@@ -101,7 +101,7 @@ export default function Manual(props: any) {
                         {
                             currency.toUpperCase() === 'XRP'
                                 ? <p className="text-center m-0 -mt-1 font-semibold underline flex items-center justify-center gap-2 text-primary" onClick={() => { props.toggleMarkdownURL('https://raw.githubusercontent.com/XRPL-Labs/Help-Center/main/getting-started/how-to-activate-a-new-xrpl-account.md') }}>More information <span><img className="m-0" src={externalLinkIcon} /></span></p>
-                                : <p className="text-center m-0 -mt-1 font-semibold underline flex items-center justify-center gap-2 text-primary" onClick={() => { props.toggleMarkdownURL('https://raw.githubusercontent.com/XRPL-Labs/Help-Center/main/getting-started-with-xumm/activating-an-account-xahau/README.md') }}>More information <span><img className="m-0" src={externalLinkIcon} /></span></p>
+                                : <p className="text-center m-0 -mt-1 font-semibold underline flex items-center justify-center gap-2 text-primary" onClick={() => { props.toggleMarkdownURL('https://raw.githubusercontent.com/XRPL-Labs/Help-Center/main/getting-started-with-xumm/activating-a-xahau-account/README.md') }}>More information <span><img className="m-0" src={externalLinkIcon} /></span></p>
                         }
 
                         <div className="fixed bottom-0 w-full -ml-5 pl-[20px] pr-[22px] pb-8 z-10 bg-transparent">
