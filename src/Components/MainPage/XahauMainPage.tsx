b702a584f80797ff04357819a4daaaa21707bbc9
Kevin Koobs <k.koobs@outlook.com>
2024-10-17 12:41:22 +0200

    fix(activation): Add bearer to page

--- src/Components/MainPage/XahauMainPage.tsx
+++ src/Components/MainPage/XahauMainPage.tsx
@@ -6,7 +6,7 @@ import { Error as ErrorComponent } from '../Error';
 
 const Loader = lazy(() => import('./Loader'));
 
-async function fundXahauAccount({ xAppToken, bearer }: { xAppToken: string, bearer: string }): Promise<boolean | any> {
+async function fundXahauAccount(xAppToken: string, bearer: string): Promise<boolean | any> {
     const activationRequest = await fetch(`${import.meta.env.VITE_XAPP_TANGEM_ENDPOINT}${xAppToken}/direct`, {
         method: "POST",
         headers: {
